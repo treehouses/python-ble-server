f0a55bf4f449650c184839bee10d0b1b7e109c20
roshan <rijalroshan94@gmail.com>
2021-07-24 03:27:54 +0000

    cleanup

--- serverble.py
+++ serverble.py
@@ -17,38 +17,38 @@ UART_RX_CHARACTERISTIC_UUID =  '6e400002-b5a3-f393-e0a9-e50e24dcca9e'
 UART_TX_CHARACTERISTIC_UUID =  '6e400003-b5a3-f393-e0a9-e50e24dcca9e'
 mainloop = None
  
-class TxCharacteristic(Characteristic):
-    def __init__(self, bus, index, service):
-        Characteristic.__init__(self, bus, index, UART_TX_CHARACTERISTIC_UUID,
-                                ['notify'], service)
-        self.notifying = False
-        GLib.io_add_watch(sys.stdin, GLib.IO_IN, self.on_console_input)
+# class TxCharacteristic(Characteristic):
+#     def __init__(self, bus, index, service):
+#         Characteristic.__init__(self, bus, index, UART_TX_CHARACTERISTIC_UUID,
+#                                 ['notify'], service)
+#         self.notifying = False
+#         GLib.io_add_watch(sys.stdin, GLib.IO_IN, self.on_console_input)
  
-    def on_console_input(self, fd, condition):
-        s = fd.readline()
-        if s.isspace():
-            pass
-        else:
-            self.send_tx(s)
-        return True
+#     def on_console_input(self, fd, condition):
+#         s = fd.readline()
+#         if s.isspace():
+#             pass
+#         else:
+#             self.send_tx(s)
+#         return True
  
-    def send_tx(self, s):
-        if not self.notifying:
-            return
-        value = []
-        for c in s:
-            value.append(dbus.Byte(c.encode()))
-        self.PropertiesChanged(GATT_CHRC_IFACE, {'Value': value}, [])
+#     def send_tx(self, s):
+#         if not self.notifying:
+#             return
+#         value = []
+#         for c in s:
+#             value.append(dbus.Byte(c.encode()))
+#         self.PropertiesChanged(GATT_CHRC_IFACE, {'Value': value}, [])
  
-    def StartNotify(self):
-        if self.notifying:
-            return
-        self.notifying = True
+#     def StartNotify(self):
+#         if self.notifying:
+#             return
+#         self.notifying = True
  
-    def StopNotify(self):
-        if not self.notifying:
-            return
-        self.notifying = False
+#     def StopNotify(self):
+#         if not self.notifying:
+#             return
+#         self.notifying = False
  
 class RxCharacteristic(Characteristic):
     result = None
